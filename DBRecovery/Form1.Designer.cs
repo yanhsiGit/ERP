8105b52a0005f285a6598871feb9d2246f7f6cba
Alex Lee <leyanhsi@gmail.com>
2017-12-17 15:16:14 +0800

    Add files via upload

---
+++ DBRecovery/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace DBRecovery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDBName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtComputerName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Controls.Add(this.txtPath);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtDBName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtComputerName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(33, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(640, 113);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "資料輸入區";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(579, 70);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(55, 23);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "瀏覽...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(109, 70);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(461, 22);
+            this.txtPath.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "備份或還原路徑";
+            // 
+            // txtDBName
+            // 
+            this.txtDBName.Location = new System.Drawing.Point(140, 44);
+            this.txtDBName.Name = "txtDBName";
+            this.txtDBName.Size = new System.Drawing.Size(242, 22);
+            this.txtDBName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "備份或還原資料庫名稱";
+            // 
+            // txtComputerName
+            // 
+            this.txtComputerName.Location = new System.Drawing.Point(202, 19);
+            this.txtComputerName.Name = "txtComputerName";
+            this.txtComputerName.Size = new System.Drawing.Size(180, 22);
+            this.txtComputerName.TabIndex = 1;
+            this.txtComputerName.Text = "LocalHost";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請輸入SQL Server所在電腦名稱或IP";
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Image = global::DBRecovery.Properties.Resources.Recovery;
+            this.btnRestore.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRestore.Location = new System.Drawing.Point(127, 22);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(75, 36);
+            this.btnRestore.TabIndex = 13;
+            this.btnRestore.Text = "還原";
+            this.btnRestore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Image = global::DBRecovery.Properties.Resources.Cancel;
+            this.btnCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancel.Location = new System.Drawing.Point(220, 22);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 36);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Image = global::DBRecovery.Properties.Resources.Backup;
+            this.btnBackup.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBackup.Location = new System.Drawing.Point(33, 22);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(75, 36);
+            this.btnBackup.TabIndex = 11;
+            this.btnBackup.Text = "備份";
+            this.btnBackup.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 211);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnBackup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "資料庫備份與還原";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDBName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtComputerName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ DBRecovery/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace DBRecovery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+            folderBrowserDialog1.SelectedPath = @"C:\";
+            folderBrowserDialog1.ShowNewFolderButton = true;
+            btnBackup.Enabled = false;
+            btnRestore.Enabled = false;
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            if (txtComputerName.Text =="" || txtDBName.Text =="" || txtPath.Text =="")
+            {
+                MessageBox.Show(this, "輸入區欄位內容均不得為空白!!", "欄位驗證");
+                return;
+            }
+            string MessageString = "";
+            string Caption = "";
+            MessageBoxButtons Buttons;
+            MessageBoxIcon Icons;
+            DialogResult DR;
+
+            MessageString = "是否要進行資料庫備份作業?";
+            Caption = "資料備份";
+            Buttons = MessageBoxButtons.YesNo;
+            Icons = MessageBoxIcon.Information;
+            DR = MessageBox.Show(MessageString, Caption, Buttons, Icons);
+
+            if (DR == DialogResult.Yes)
+            {
+                string extension = ".bak"; //備份資料庫副檔名
+                string dbName = txtDBName.Text;
+                if (txtPath.Text.Substring(txtPath.Text.Length - 1, 1) != @"\")
+                {
+                    dbName = @"\" + dbName;
+                }
+                string argument = string.Format(@"-E -S {0} -Q  ""BACKUP DATABASE {1} TO DISK='{2}{3}{4}'""" ,
+    txtComputerName.Text, txtDBName.Text, txtPath.Text, dbName, extension);
+                // append user/password if not use integrated security
+                // argument += string.Format(" -U {0} -P {1}", User, Password);
+                var process = Process.Start("sqlcmd.exe", argument);
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+                while (true)
+                {
+                    // wait for the process exits. The WaitForExit() method doesn't work
+                    if (process.HasExited)
+                        break;
+                        System.Threading.Thread.Sleep(500);
+                }
+                string msg = "資料庫[" + txtDBName.Text + "]備份成功!" + "\r\n";
+                msg = msg + "備份路徑為:" + txtPath.Text+ dbName+ extension;
+                MessageBox.Show(this,msg , "資訊");
+             }
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            if (txtComputerName.Text == "" || txtDBName.Text == "" || txtPath.Text == "")
+            {
+                MessageBox.Show(this, "輸入區欄位內容均不得為空白!!", "欄位驗證");
+                return;
+            }
+            string MessageString = "";
+            string Caption = "";
+            MessageBoxButtons Buttons;
+            MessageBoxIcon Icons;
+            DialogResult DR;
+
+            MessageString = "是否要進行資料庫還原作業?";
+            Caption = "資料還原";
+            Buttons = MessageBoxButtons.YesNo;
+            Icons = MessageBoxIcon.Information;
+            DR = MessageBox.Show(MessageString, Caption, Buttons, Icons);
+
+            if (DR == DialogResult.Yes)
+            {
+                string extension = ".bak"; //備份資料庫副檔名
+                string dbName = txtDBName.Text;
+                if (txtPath.Text.Substring(txtPath.Text.Length - 1, 1) != @"\")
+                {
+                    dbName = @"\" + dbName;
+                }
+                string DbPath = txtPath.Text + dbName + extension;
+                if (File.Exists(DbPath) == false)
+                {
+                    MessageBox.Show(DbPath + "資料庫檔案不存在!!無法進行還原!!", "警告");
+                    return;
+                }
+
+                string argument = string.Format(@"-E -S {0} -Q  ""RESTORE DATABASE {1} FROM DISK='{2}'""",
+                 txtComputerName.Text, txtDBName.Text, DbPath);
+                var process = Process.Start("sqlcmd.exe", argument);
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+                while (true)
+                {
+                    // Wait for the process exits. The WaitForExit() method doesn't work
+                    if (process.HasExited)
+                        break;
+                    System.Threading.Thread.Sleep(500);
+                }
+                string msg = "資料庫[" + txtDBName.Text + "]還原成功!" + "\r\n";
+                msg = msg + "還原路徑為:" + txtPath.Text + dbName + extension;
+                MessageBox.Show(this, msg, "資訊");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            txtPath.Text = folderBrowserDialog1.SelectedPath;
+            if (txtPath.Text != "")
+            {
+                btnBackup.Enabled = true;
+                btnRestore.Enabled = true;
+            }
+        }
+    }
+}

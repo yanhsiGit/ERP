48bda1a6bebb79af621c75236f714455bf141595
Alex Lee <leyanhsi@gmail.com>
2017-12-17 15:33:35 +0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 組件的一般資訊是由下列的屬性集控制。
+// 變更這些屬性的值即可修改組件的相關
+// 資訊。
+[assembly: AssemblyTitle("超級進銷存系統(Super Invoicing System , SIS)")]
+[assembly: AssemblyDescription("提供採購進貨(Procurement)、銷售出貨(Sales)、存貨管理(Stock)功能，此軟體由許清榮開發，本人保留此軟體所有權利，有任何問題可Email至 ching.rung@msa.hinet.net 與我聯絡。")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("博碩科技")]
+[assembly: AssemblyProduct("超級進銷存系統(SIS)")]
+[assembly: AssemblyCopyright("Copyright (C) 2014~2016 by Ryu. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 將 ComVisible 設定為 false 會使得這個組件中的類型
+// 對 COM 元件而言為不可見。如果您需要從 COM 存取這個組件中
+// 的類型，請在該類型上將 ComVisible 屬性設定為 true。
+[assembly: ComVisible(false)]
+
+// 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+[assembly: Guid("526bed94-8618-48a9-869c-9735b9548ab3")]
+
+// 組件的版本資訊是由下列四項值構成: 
+//
+//      主要版本
+//      次要版本 
+//      組建編號
+//      修訂編號
+//
+// 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
+// 指定為預設值: 
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
